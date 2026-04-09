7a21645ed862d05aa52e253fe64f3f74262df037
j4yesh-911 <jayeshgayri12@gmail.com>
2026-04-09 14:39:47 +0530

    api.js update

--- FRONTEND/src/services/api.js
+++ FRONTEND/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:VITE_API_URL ||"http://localhost:5000/api",
+  baseURL:import.meta.env.VITE_API_URL ||"http://localhost:5000/api",
 });
 
 // Track if we're already handling logout to prevent infinite loops
