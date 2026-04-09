21c943a30211f445d759bf263ca29edbacca2811
j4yesh-911 <jayeshgayri12@gmail.com>
2026-04-09 14:32:47 +0530

    api.js fix

--- FRONTEND/src/services/api.js
+++ FRONTEND/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:" https://skitzoo-1.onrender.com" ||"http://localhost:5000/api",
+  baseURL:VITE_API_URL ||"http://localhost:5000/api",
 });
 
 // Track if we're already handling logout to prevent infinite loops
